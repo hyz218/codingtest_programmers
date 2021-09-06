4b763656c6239c4b15af5ef3e51dc2ab6b5b7255
hyz218 <dlsal1890@naver.com>
2021-09-06 13:44:39 +0900

    Delete incorrectly uploaded files

--- dot_product_list.py
+++
@@ -1,9 +0,0 @@
-#내적
-#길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
-#이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
-
-def solution(a, b):
-    answer = 0 #answer 초기화
-    for i in range(len(a)):#두 배열의 길이가 같으므로 range를 len(a)로 지정
-        answer = answer + a[i]*b[i] #answer에 내적계산 update
-    return answer
--- sum_of_each_digit_num.py
+++
@@ -1,8 +0,0 @@
-#자릿수 더하기
-#자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
-#예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
-
-def solution(n):
-    answer = list(map(int,str(n))) #입력받은 정수를 list형태로 변환
-    answer = sum(answer) #list값의 합계
-    return answer
